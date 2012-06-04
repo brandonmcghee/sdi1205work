ed4d761218fe39b3862db5aa7ed81800cbbb936c
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-06-03 21:06:54 -0400

    Changed the JS file to add loops.

--- mcghee_brandon_assignment2.js
+++ mcghee_brandon_assignment2.js
@@ -3,4 +3,14 @@
 var boys = ["Brandon", "Mike", "Chuck"];
 var activities = ["Basketball", "Bamboo Quest"];
 var current = false;
-)
+var eod = 5;
+
+while (eod > 0) {
+    console.log(boys[0] + " is running away from the dogs.");
+    eod--;
+    
+        for (i = 0; i < 3;  i++) {
+            console.log(boys[1] + " and " + boys[2] + " jumped the fence.");
+        };
+};
+
