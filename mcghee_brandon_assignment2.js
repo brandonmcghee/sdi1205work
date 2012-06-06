3070640607609c7ffb08e82a49340183dda96ec3
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-06-05 20:30:03 -0400

    Trying out While loop for current variable.

--- mcghee_brandon_assignment2.js
+++ mcghee_brandon_assignment2.js
@@ -1,5 +1,21 @@
 //This Javascript developed by Brandon McGhee for SDI 1205 Project 2
 
+var boys = ["Brandon", "Mike", "Chuck"];
+var vote = 0;
+var current = true;
+var powerOut = 0;
+var zelda = "One day three friends were playing The Legend of Zelda. They were getting close to defeating gannon when suddenly. Click! The power shuts off and the boys lose their game. ";
+             
 
+
+while (current) {
+    console.log(zelda);
+    powerOut++;
+    
+        if (powerOut > 0) {
+            current = false;
+        }
+    
+}
 
 
