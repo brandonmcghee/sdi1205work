fd81f66950c6c433d4057748bff348abd771d851
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-06-05 20:47:59 -0400

    Setting up structure of program from flowchart example.

--- mcghee_brandon_assignment2.js
+++ mcghee_brandon_assignment2.js
@@ -3,19 +3,41 @@
 var boys = ["Brandon", "Mike", "Chuck"];
 var vote = 0;
 var current = true;
-var powerOut = 0;
 var zelda = "One day three friends were playing The Legend of Zelda. They were getting close to defeating gannon when suddenly. Click! The power shuts off and the boys lose their game. ";
-             
+
+var story = function() {
+    
+    powerOut();
+    
+};//End adventure Function
+
+var powerOut = function() {
+    
+    checkBreaker();
+    
+};//End powerOut Function
+
+var checkBreaker = function() {
+    
+    decideActivity();
+    
+}//End checkBreaker Function
+
+var decideActivity = function(activity,vote) {
+    
+    console.log("Since there is no current and the power will be out for a while the boys must do something else.")
+    console.log(boy[2] + " suggested that they play Basketball since it was so nice outside today.");
+    console.log(boy[1] + " had another idea to go look for some Bamboo in the woods he heard about.");
+    console.log("All three of them decided to put it to a vote.");
+    
+    
+};//End decideActivity Function
+
+console.log(zelda);
+story();
 
 
-while (current) {
-    console.log(zelda);
-    powerOut++;
     
-        if (powerOut > 0) {
-            current = false;
-        }
-    
-}
+
 
 
