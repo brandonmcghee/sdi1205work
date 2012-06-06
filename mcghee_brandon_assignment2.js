36a8b12f2c8f1d2f6787b2e6d46dd23a12b5dc4c
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-06-05 20:04:04 -0400

    Starting from scratch on the Javascript file.

--- mcghee_brandon_assignment2.js
+++ mcghee_brandon_assignment2.js
@@ -1,19 +1,5 @@
 //This Javascript developed by Brandon McGhee for SDI 1205 Project 2
 
-var boys = ["Brandon", "Mike", "Chuck"];
-var zelda = [
-             "A group of friends were enjoying a great game of Legend of Zelda one day.  ",
-             " was playing and ",
-             " were cheering him on.  ",
-             "Suddenly the power went out and the game was lost!"
-             ];
-var activities = ["Basketball", "Bamboo Quest"];
-var current = true;
-var powerfailure = 4;
 
-
-for (var i = 0; i < powerfailure; i++) {
-    console.log(zelda[i]);
-};
 
 
