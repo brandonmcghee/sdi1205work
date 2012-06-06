d4c53b9bd0d9bfbbd5350aee45041ecbbbcfdb36
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-06-05 23:24:49 -0400

    Changing things around to work correctly.

--- mcghee_brandon_assignment2.js
+++ mcghee_brandon_assignment2.js
@@ -2,66 +2,78 @@
 
 var boys = ["Brandon", "Mike", "Chuck"];
 var vote = 0;
-
 var zelda = "One day three friends were playing The Legend of Zelda. They were getting close to defeating gannon when suddenly. Click! The power shuts off and the boys lose their game. ";
-
-var story = function() {
-    console.log(zelda);
-    powerOut(false);
-    
-};//End adventure Function
-
-var powerOut = function(powerOff) {
-    
-    if (powerOff) {
-        console.log(boys[0] + ", " + boys[1] + " and " + boys[2] + " had to find out the problem.  So they went and checked the breaker box");
-        checkBreaker(true);
-    }
-    else {
-        console.log("The power came back on and the boys kept playing and eventually defeated Ganon!");
-    };
-    
-    checkBreaker(false);
-    
-};//End powerOut Function
 
 var checkBreaker = function(breakerProblem) {
     
     if (breakerProblem) {
-        console.log(boys[1] + " checked the Breaker box was able to fix the problem.  So the boys defeated that pesky ole Ganon!")
+        return true;
     }
     else {
-        console.log("The breaker was fine so there is no current to the home.  Since there is no current and the power will be out for a while the boys must do something else.");
-        decideActivity("Basketball", "Bamboo Quest");
-    };
+        return false;
+    }
     
     
-}//End checkBreaker Function
+};//End checkBreaker Function
 
-var decideActivity = function() {
+var suggestActivity = function(numberOfActivities) {
+
+    while (numberOfActivities > 1) {
+        console.log("The boys narrowed down their choice of activites to " + numberOfActivities);
+        numberOfActivities--;
+    }
     
-    console.log(boys[2] + " suggested that they play Basketball since it was so nice outside today.");
-    console.log(boys[1] + " had another idea to go look for some Bamboo in the woods he heard about.");
-    console.log("All three of them decided to put it to a vote.");
-    
-    letsDiscussIt();
+    return 2;
     
 };//End decideActivity Function
 
 var letsDiscussIt = function() {
-  
-    Vote();
+    
+    var activity1 = "Basketball";
+    var activity2 = "Bamboo";
+    return ("They decided to go search for " + activity2);
     
 };//End letsDiscussIt Function
 
 var Vote = function() {
     
+    var Votes = [];
+    
+    for (var votesLeft = 3; votesLeft > 0; votesLeft--) {
+        Votes.push(1);
+    };
+    
+
+    
+    return Votes;
     
 };//End Vote Function
 
-story();
+var tellStory = function(breaker, activity, discussion, story) {
+    
+    checkBreaker(false);
+    breaker = checkBreaker();
+    console.log(breaker);
+    
+    suggestActivity(10);
+    activity = suggestActivity();
+    console.log(activity);
+    
+    letsDiscussIt();
+    discussion = letsDiscussIt();
+    console.log(discussion);
+    
+    Vote();
+    story = Vote();
+    console.log(story);
+    
+    
+    
+    
+};
 
 
+tellStory();
     
 
 
