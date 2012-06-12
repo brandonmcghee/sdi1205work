4077c3bf7a35e7d0975f5103c54981fe7c8361a2
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-06-12 11:35:55 -0400

    adding all files

---
+++ script.js
@@ -0,0 +1,24 @@
+//Brandon McGhee Assignment 3
+
+var boys = {
+    "mike" : {
+        name: "Mike",
+        vote: "Bamboo",
+        age: 11 
+    },
+    "chuck" : {
+        name: "Chuck",
+        vote: "Bamboo",
+        age: 12  
+    },
+    "brandon" : {
+        
+        name: "Chuck",
+        vote: "Bamboo",
+        age: 11
+    }
+};
+
+var activity = function (choice) {
+    
+}
