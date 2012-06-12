140915aa42ff4a238c88997cf88f415ca024daf6
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-06-12 12:27:16 -0400

    Constructed the Boys via Constructor Method

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 //Brandon McGhee Assignment 3
 
-var boys = {
-    "mike" : {
-        name: "Mike",
-        vote: "Bamboo",
-        age: 11 
-    },
-    "chuck" : {
-        name: "Chuck",
-        vote: "Bamboo",
-        age: 12  
-    },
-    "brandon" : {
-        
-        name: "Chuck",
-        vote: "Bamboo",
-        age: 11
-    }
+var boyBuilder = function (name, age, skills, suggestion) {
+    
+    var accessName = function() { return name;}
+    var accessAge = function() { return age;}
+    var accessSkills = function() { return skills;}
+    var accessSuggestion = function() { return suggestion}
+    
+    return {
+        "name": accessName,
+        "age": accessAge,
+        "skills": accessSkills,
+        "suggestion": accessSuggestion
+    };
 };
 
-var activity = function (choice) {
-    
-}
+var boy1 = boyBuilder("Mike", 11, "Ninja Skills", "Bamboo Quest");
+var boy2 = boyBuilder("Chuck", 12, "Electrical Skills", "Basketball");
+var boy3 = boyBuilder("Brandon", 11, "Computer Hacking Skills", "Don't Care");
+
+console.log(boy1.age());
+
