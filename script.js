3da635a609712cd9713c280240bd8b133b36e802
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-06-12 21:12:48 -0400

    Added a for loop to list the boys out from JSON data.'

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
 //Brandon McGhee Assignment 3
 
-var jsonData = {
-    "boys": {
-            "mike": {
-                "name": "",
-                "age": null,
-                "skills": "",
-                "activities": []
-            },
-            "chuck": {
-                "name": "",
-                "age": null,
-                "skills": "",
-                "activities": []
-            },
-            "brandon": {
-                "name": "",
-                "age": null,
-                "skills": "",
-                "activities": []
-            }
-            
+var current = false;
+
+var powerOut = function(power) {
+    if (power) {
+        console.log("The three boys played all day until they defeated Ganon!");
+    }
+    else {
+        console.log("The boys had to go check the breaker box to see what the problem was. ");
+        
+        if (current) {
+            console.log("They reset the breaker, power came back on and started the game over again!");
+        }
+        else {
+            console.log("The current is out to the house, so the boys must think of something else to do");
+        }
     }
 };
 
-
-var boyBuilder = function (Name) {
+/*var boyBuilder = function (Name) {
   
     var _Name = function() {
         return Name;
@@ -52,7 +45,9 @@ var boyBuilder = function (Name) {
         "Skills": _Skills,
         "setSkills": setSkills
     };
-};
+};*/
+
+
 
 var mike = boyBuilder("Mike");
 mike.setAge(11);
@@ -61,24 +56,42 @@ mike.setSkills("Ninja");
 console.log(mike.Name() + " is " + mike.Age() + " years old and has nice " + mike.Skills() + " skills.");
 
 var chuck = boyBuilder("Chuck");
-mike.setAge(12);
-mike.setSkills("Electrical");
-
-console.log(chuck.Name() + " is " + chuck.Age() + " years old and has nice " + chuck.Skills() + " skills.");
+mike.setAge(12);//setter
+mike.setSkills("Electrical");//setter
 
 var brandon = boyBuilder("Brandon");
 mike.setAge(11);
 mike.setSkills("Computer Hacking");
 
-console.log(brandon.Name() + " is " + brandon.Age() + " years old and has nice " + brandon.Skills() + " skills.");
+var heroes = {
+    "boys": {
+            "mike": {
+                "name": "Mike",
+                "age": 11,
+                "skills": "Ninja",
+                "activities": ["Bamboo"]
+            },
+            "chuck": {
+                "name": "Chuck",
+                "age": 12,
+                "skills": "Electrial",
+                "activities": ["Basketball", "Cards", "Swimming"]
+            },
+            "brandon": {
+                "name": "Brandon",
+                "age": 11,
+                "skills": "Computer Hacking",
+                "activities": ["Basketball", "Cards", "Frizbee"]
+            }
+            
+    }
+};
 
-console.log(brandon.Name());
-console.log(chuck.Name());
+powerOut(true);
 
+//getter method (accessor)
+for (var key in heroes.boys) {
+    var boy = heroes.boys[key];
+    console.log(boy.name + " is " + boy.age + " years old and has " + boy.skills + " skills.");
+};
 
-
-jsonData.boys.brandon.age = brandon.Age();
-jsonData.boys.brandon.skills = brandon.Skills();
-
-console.log(jsonData.boys.brandon.age);
-console.log(jsonData.boys.brandon.skills);
