07eff0b3e5dc79a0f44162773073cc4afd2113eb
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-06-14 13:12:50 -0400

    Updating functions to pass variables

--- script.js
+++ script.js
@@ -34,36 +34,15 @@ var beginning = [
 "They decided to go check out the breaker box to see what was wrong. ",         
 ];
 
+//Array Function - For Loop
 var intro = function() {
     for (var i = 0; i < beginning.length; i++) {
             console.log(beginning[i]);
     }
-    powerOut(false);
+    powerOut(false);//Calling Boolean Function, passing Boolean Value
 }
 
-var chuckSkills = function () {
-    if (json.boys.chuck.skills == "Electrical") {
-        console.log(json.boys.chuck.name + " knows his stuff and goes to work on the problem.");
-        iterator = 10;
-    }
-    else {
-        console.log(chuck + " does not remember how to fix this problem.");
-    }
-    killingTime(5)
-};
-
-var killingTime = function(timeLeft) {
-    while (timeLeft > 0) {
-        console.log(chuck + " said it would be " + timeLeft + " minutes before he was done.");
-        while (timeLeft > 3) {
-                console.log(mike + " and " + brandon + " sat and waited for " + chuck + " to fix the problem");
-                timeLeft--;
-        }
-        console.log("The boys were starting to get impatient with " + chuck);
-    }
-    
-};
-
+//Boolean Function - Nested Conditional
 var powerOut = function(power) {
     if (power) {
         console.log(brandon + ", " + mike + " and " + chuck + " played all day until they defeated Ganon!");
@@ -71,68 +50,48 @@ var powerOut = function(power) {
     else {
         console.log("The boys had to go check the breaker box to see what the problem was.  They will need someone with Electrical skills to fix this.");
         console.log(chuck + " is one of the few people who knows Electrical stuff, so they ask him if he knows how to fix it.");
-        chuckSkills();
+        chuckSkills("knows his stuff and goes to work on the problem");//Calling Object Function, passing String
         
         if (current) {
             console.log(chuck + " reset the breaker, power came back on and started the game over again!");
         }
         else {
-            console.log(chuck + " determined that the current is out to the house, so the boys must think of something else to do");
+            return
         }
     }
+    
 };
 
-
-
-/*var boyBuilder = function (Name) {
-  
-    var _Name = function() {
-        return Name;
+//Object Function
+var chuckSkills = function (knowledge) {
+    if (json.boys.chuck.skills == "Electrical") { //Getter Method
+        console.log(json.boys.chuck.name + " " + knowledge);
     }
-    var setAge = function(num) {
-        age = num;
+    else {
+        console.log(chuck + " does not remember how to fix this problem.");
     }
-    
-    var _Age = function() {
-        return age;
+    killingTime(8)//Call Number Function, Passing Number (8) minutes to work on Electrical Problem
+};
+
+//Number Function - Nested Loop
+var killingTime = function(timeLeft) {
+    var timePassed = 1;
+    while (timeLeft > 0) {
+            if (timeLeft == 8) {
+                console.log(chuck + " said it would be " + timeLeft + " minutes before he was done.");
+            }
+
+        while (timePassed > 0 && timePassed < 8) {
+                console.log(mike + " and " + brandon + " were bored and impatient with " + chuck + " and it had been " + timePassed + " minutes since Chuck started.");
+                timePassed++;
         }
-        
-    var setSkills = function(item) {
-        skills = item;
+            timeLeft--;
     }
-    var _Skills = function() {
-        return skills;
-        }
+    console.log(chuck + " determined that the current is out to the house, so the boys must think of something else to do");
+
+    return
     
-    return {
-        "Name": _Name,
-        "Age": _Age,
-        "setAge": setAge,
-        "Skills": _Skills,
-        "setSkills": setSkills
-    };
-};*/
+};
 
-/*var mike = boyBuilder("Mike");
-mike.setAge(11);
-mike.setSkills("Ninja");
-
-console.log(mike.Name() + " is " + mike.Age() + " years old and has nice " + mike.Skills() + " skills.");
-
-var chuck = boyBuilder("Chuck");
-mike.setAge(12);//setter
-mike.setSkills("Electrical");//setter
-
-var brandon = boyBuilder("Brandon");
-mike.setAge(11);
-mike.setSkills("Computer Hacking");
-*/
-
-
-//getter method (accessor)
-/*for (var key in json.boys) {
-    var boy = json.boys[key];
-    console.log(boy.name + " is " + boy.age + " years old and has " + boy.skills + " skills.");
-};*/
 
 intro();
