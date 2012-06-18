96161717c1f085ebebdbe35da98f725848f43aa3
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-06-18 17:30:02 -0400

    Testing out the Phone Validate Function, trying to get the logic together

--- script.js
+++ script.js
@@ -4,16 +4,27 @@
 var stringFunc = function() {
     
     var isPhoneNum = function(phoneNum) {
-
+        
+        var dash = "-";
+        
+        if (phoneNum.charAt(3) == dash && phoneNum.charAt(7) == dash) {
+            console.log("Valid Phone Number")
+            if (phoneNum.length == 11) {
+                console.log("Correct Length");
+            }
+        }
+        else {
+            console.log("Invalid Phone Number");
+        }
     }
     
     return {
-        "isPhoneNum": isPhoneNum
+        "isPhone": isPhoneNum
     }
     
 };
 
 var inputPhone = "407-555-1234";
-var PHONE = stringFunctions("phoneValidate");
+var PHONE = stringFunc("phoneValidate");
 
 PHONE.isPhone(inputPhone);
