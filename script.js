9aeac4d0bb2984f0ff13c035597e56fa9f1e0506
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-06-18 17:19:25 -0400

    Setting up structure of first String Function to validate Phone Numbers

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 //Brandon McGhee
 //SDI1205 Project 4 "Building a Library"
 
+var stringFunc = function() {
+    
+    var isPhoneNum = function(phoneNum) {
+
+    }
+    
+    return {
+        "isPhoneNum": isPhoneNum
+    }
+    
+};
+
+var inputPhone = "407-555-1234";
+var PHONE = stringFunctions("phoneValidate");
+
+PHONE.isPhone(inputPhone);
